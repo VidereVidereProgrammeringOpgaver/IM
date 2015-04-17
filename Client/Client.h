102695adc8ffd47f77bce438fab9b6ec2e95734e
Martin Vieth <martin_123_vieth@hotmail.com>
2015-04-17 10:40:00 +0200

    First Commit

---
+++ Client/Client.h
@@ -0,0 +1,17 @@
+/*
+ * Client.h
+ *
+ *  Created on: 17/04/2015
+ *      Author: clausstaffe
+ */
+
+#ifndef CLIENT_H_
+#define CLIENT_H_
+
+class Client {
+public:
+	Client();
+	virtual ~Client();
+};
+
+#endif /* CLIENT_H_ */
